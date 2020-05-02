96d6b3d3f298cef24938cd17c861a5a56409911b
davidojes <ojesekho@sheridancollege.ca>
2020-05-02 19:23:02 -0400

    Site now has a fair amount of structure and design implemented.

---
+++ js/index.js
@@ -0,0 +1,8 @@
+/* pure css navbar */
+function classToggle() {
+    const navs = document.querySelectorAll('.navbar-items')
+    navs.forEach(nav => nav.classList.toggle('navbar-toggleshow'));
+  }
+  
+  document.querySelector('.navbar-link-toggle')
+    .addEventListener('click', classToggle);
